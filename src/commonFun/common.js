6e6700747395cf48daf9c46bbc09888450603449
hellovue2017 <gouxiaojun@shenguyun.com>
2017-06-30 18:47:01 +0800

    add

--- src/commonFun/common.js
+++ src/commonFun/common.js
@@ -1,7 +1,16 @@
 import StringUtil from "./stringUtil"
+import arrayUtil from "./arrayUtil"
+import commonlyFun from "./commonlyFun"
+import judgeEmptyUtil from "./judgeEmptyUtil"
+import objectUtil from "./objectUtil"
 import typeJudgment from "./typeJudgment"
 
+
 export default {
-	StringUtil,
-	typeJudgment
+    StringUtil,
+    arrayUtil,
+    commonlyFun,
+    judgeEmptyUtil,
+    objectUtil,
+    typeJudgment
 }
