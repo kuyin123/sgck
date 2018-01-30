08272603d399ec56af7dc015e0e1e5a6379c3aa5
hyb <539969654@qq.com>
2018-01-30 13:49:53 +0800

    rebuild

--- dist/sgck.js
+++ dist/sgck.js
@@ -608,85 +608,9 @@ exports.default = {
 
 /***/ }),
 /* 8 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-/*
-* sg-utils-dateHandler 2017-06
-* date handler utils
-* */
-
-/* export 的顶层对象 */
-var dateHandler = {
-  prevDay: prevDay,
-  prevWeek: prevWeek,
-  prevMonth: prevMonth,
-  prevYear: prevYear,
-  dateFormat: dateFormat
-};
-
-/*
-* Date 的处理方法和格式化
-* 传入要格式化的时间数据, 返回相应格式的时间值.
-* */
-// 1. 前一天
-function prevDay(src) {
-  return new Date(src.getTime() - 24 * 60 * 60 * 1000);
-};
-// 2. 前一周
-function prevWeek(src) {
-  return new Date(src.getTime() - 7 * 24 * 60 * 60 * 1000);
-};
-// 3. 前一月
-function prevMonth(src) {
-  var year = src.getFullYear();
-  var month = src.getMonth();
-  var date = src.getDate();
-  var newYear = month === 0 ? year - 1 : year;
-  var newMonth = month === 0 ? 11 : month - 1;
-  var newMonthDayCount = new Date(newYear, newMonth + 1, 0).getDate(); //getDayCountOfMonth(newYear, newMonth);
-  if (newMonthDayCount < date) {
-    src.setDate(newMonthDayCount);
-  }
-  src.setMonth(newMonth);
-  src.setFullYear(newYear);
-  return new Date(src.getTime());
-};
-// 4. 前一年
-function prevYear(src) {
-  var date = new Date(src);
-  date.setFullYear(date.getFullYear() - 1);
-  return date;
-};
-// 5. 日期根据传入条件格式化
-function dateFormat(date, fmt) {
-  var o = {
-    "M+": date.getMonth() + 1, //月份
-    "d+": date.getDate(), //日
-    "h+": date.getHours(), //小时
-    "m+": date.getMinutes(), //分
-    "s+": date.getSeconds(), //秒
-    "q+": Math.floor((date.getMonth() + 3) / 3), //季度
-    "S": date.getMilliseconds() //毫秒
-  };
-  if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
-  }
-  for (var k in o) {
-    if (new RegExp("(" + k + ")").test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
-    }
-  }
-  return fmt;
-};
-
-/* 暴露顶层对象 */
-exports.default = dateHandler;
+throw new Error("Module build failed: SyntaxError: D:/sgck/src/dateHandler/dateHandler.js: Unexpected token, expected ; (77:12)\n\n  75 |    *@time:Date\n  76 |    */\n> 77 |   let result: string\n     |             ^\n  78 | \n  79 |   const format = (value) => {\n  80 |     return value >= 10 ? value : '0' + value\n");
 
 /***/ }),
 /* 9 */
